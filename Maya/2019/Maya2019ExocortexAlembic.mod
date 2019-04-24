657e6b19c7e7784c02cc310fc514ef725d8625fd
liorbenhorin <liorbenhorin@gmail.com>
2019-04-24 19:06:26 +0300

    maya 2019

---
+++ Maya/2019/Maya2019ExocortexAlembic.mod
@@ -0,0 +1,1 @@
++ Maya2019ExocortexAlembic 1.0 C:/ExocortexAlembic/Maya2019/Module
--- _build/build-vs2015_x64.bat
+++ _build/build-vs2015_x64.bat
@@ -2,5 +2,8 @@ rmdir /s vs2015_x64
 mkdir vs2015_x64
 cd vs2015_x64
 cmake -G "Visual Studio 14 Win64" -DHDF5_ENABLE_THREADSAFE=ON ..\..
+pause
+cd vs2015_x64
+cmake --build vs2015_x64
 cd ..
 pause
